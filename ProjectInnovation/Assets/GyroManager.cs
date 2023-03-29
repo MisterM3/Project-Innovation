a7ea51a0c3f1bf7ea1676134221058f0960c3f31
TygoDev <tygohaan@gmail.com>
2023-03-29 12:39:25 +0200

    Working gyroscope

---
+++ ProjectInnovation/Assets/GyroManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroManager : MonoBehaviour
+{
+    private Gyroscope phoneGyro;
+    private Quaternion correctionQuaternion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        phoneGyro = Input.gyro;
+        phoneGyro.enabled = true;
+        correctionQuaternion = Quaternion.Euler(90f, 0f, 0f);
+    }
+
+    void Update()
+    {
+        GyroModifyCamera();
+    }
+
+    void GyroModifyCamera()
+    {
+        Quaternion gyroQuaternion = GyroToUnity(Input.gyro.attitude);
+        // rotate coordinate system 90 degrees. Correction Quaternion has to come first
+        Quaternion calculatedRotation = correctionQuaternion * gyroQuaternion;
+        transform.rotation = calculatedRotation;
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
