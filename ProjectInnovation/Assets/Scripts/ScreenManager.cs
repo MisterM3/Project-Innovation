2f88c006a615b8d40fb4a4dcc7755a19e08de98d
TygoDev <tygohaan@gmail.com>
2023-04-05 11:53:25 +0200

    Basic camera movement (needs improvement)

--- ProjectInnovation/Assets/Scripts/ScreenManager.cs
+++ ProjectInnovation/Assets/Scripts/ScreenManager.cs
@@ -1,41 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ScreenManager : MonoBehaviour
 {
     DeviceOrientation phoneOrientation;
     [SerializeField] private List<Canvas> screens;
+    [SerializeField] private new CinemachineVirtualCamera camera;
+
+    private bool inPuzzle = false;
 
     void Update()
     {
-        phoneOrientation = Input.deviceOrientation;
-
-        //switch that changes the camera's rotation and active canvas based on the phone's orientation.
-        switch (phoneOrientation)
+        if (!inPuzzle)
         {
-            case DeviceOrientation.Portrait:
-                Camera.main.transform.localRotation = Quaternion.Euler(0, 0, 0);
-                SwitchCanvas(screens[0]);
-                break;
-            case DeviceOrientation.LandscapeLeft:
-                Camera.main.transform.localRotation = Quaternion.Euler(0, 0, -270);
-                SwitchCanvas(screens[1]);
-                break;
-            case DeviceOrientation.LandscapeRight:
-                Camera.main.transform.localRotation = Quaternion.Euler(0, 0, 270);
-                SwitchCanvas(screens[2]);
-                break;
-            case DeviceOrientation.PortraitUpsideDown:
-                Camera.main.transform.localRotation = Quaternion.Euler(0, 0, 180);
-                SwitchCanvas(screens[3]);
-                break;
-            default:
-                Camera.main.transform.localRotation = Quaternion.Euler(0, 0, 0);
-                SwitchCanvas(screens[0]);
-                break;
+            phoneOrientation = Input.deviceOrientation;
 
+            //switch that changes the camera's rotation and active canvas based on the phone's orientation.
+            switch (phoneOrientation)
+            {
+                case DeviceOrientation.Portrait:
+                    camera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    SwitchCanvas(screens[0]);
+                    break;
+                case DeviceOrientation.LandscapeLeft:
+                    camera.transform.localRotation = Quaternion.Euler(0, 0, -270);
+                    SwitchCanvas(screens[1]);
+                    break;
+                case DeviceOrientation.LandscapeRight:
+                    camera.transform.localRotation = Quaternion.Euler(0, 0, 270);
+                    SwitchCanvas(screens[2]);
+                    break;
+                case DeviceOrientation.PortraitUpsideDown:
+                    camera.transform.localRotation = Quaternion.Euler(0, 0, 180);
+                    SwitchCanvas(screens[3]);
+                    break;
+                default:
+                    camera.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    SwitchCanvas(screens[0]);
+                    break;
+
+            }
         }
+    }
+
+    public void SetPuzzle(bool value)
+    {
+        inPuzzle = value;
     }
 
     /// <summary>
